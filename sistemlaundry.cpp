b743df0e89b92ce1ee65abfbe2efd01c0e05eab1
farihrakan <farihrakan548@gmail.com>
2026-05-09 20:59:52 +0700

    commit pertama isi struct

---
+++ sistemlaundry.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int id;
+	string namaPelanggan;
+	string jenisLayanan;
+	float beratKg;
+	float totalHarga;
+	string status;
+	Node *next;
