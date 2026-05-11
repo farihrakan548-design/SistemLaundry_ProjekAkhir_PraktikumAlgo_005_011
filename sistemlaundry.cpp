143d36366e0bd45399f59f07da8e03660b9ded3d
farihrakan <farihrakan548@gmail.com>
2026-05-11 21:19:54 +0700

    buat node baru

--- sistemlaundry.cpp
+++ sistemlaundry.cpp
@@ -9,3 +9,32 @@ struct Node {
 	float totalHarga;
 	string status;
 	Node *next;
+};
+
+int idCounter = 1;
+
+// Buat Node Baru
+Node *buatNode(string nama, string layanan, float berat) {
+	Node *newNode = new Node;
+	newNode->id = idCounter++;
+	newNode->namaPelanggan = nama;
+	newNode->jenisLayanan = layanan;
+	newNode->beratKg = berat;
+	newNode->status = "Sedang Antri";
+	newNode->next = nullptr;
+
+	float hargaPerKg = 0;
+	if (layanan == "Cuci Kering") {
+		hargaPerKg = 5000;
+	} else if (layanan == "Cuci Setrika") {
+		hargaPerKg = 8000;
+	} else if (layanan == "Setrika Saja") {
+		hargaPerKg = 4000;
+	}
+	newNode->totalHarga = berat * hargaPerKg;
+	return newNode;
+}
+
+int main(){
+
+}
